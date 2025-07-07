cce4cfdb6eb22a423269157a6857bd266a2d33d1
Luis Paredes <lparedesr@fi.uba.ar>
2025-07-07 14:52:12 -0300

    ElasticIP (EIP) for AWS EC2 instance

---
+++ eip/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-068990796e29243d5"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+resource "aws_eip" "elastic_ip" {
+  instance = aws_instance.web.id
+  domain   = "vpc"
+}
+
+output "EIP" {
+  value = aws_eip.elastic_ip.public_ip
+}
