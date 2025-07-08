0b5ad9b18a815043f237b7c508ef8726e3f70ba6
Luis Paredes <lparedesr@fi.uba.ar>
2025-07-08 13:13:11 -0300

    modules input variables

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,8 +1,17 @@
+variable "name" {
+  type = string
+}
+
 variable "type" {
+  type    = string
   default = "t2.micro"
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-032598fcc7e9d1c7a"
+  ami           = "ami-05ffe3c48a9991133"
   instance_type = var.type
+
+  tags = {
+    Name = var.name
+  }
 }
--- modules/main.tf
+++ modules/main.tf
@@ -9,8 +9,10 @@ provider "aws" {
 # }
 
 
-module "ec2" {
-  source   = "./ec2"
+module "ec2_module" {
+  source   = "./ec2" # Everything inside ./ec2 folder is our module
   for_each = toset(["prod", "dev", "stage"])
+  name     = "my EC2 instance" # Required attribute
+  type     = "t2.micro"        # Optional attribute
 }
 
