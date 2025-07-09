5d6089032135dcd2ccb5b19d58b5165091890c44
Luis Paredes <lparedesr@fi.uba.ar>
2025-07-09 13:48:46 -0300

    updated AMI

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 
 resource "aws_instance" "web" {
-  ami           = "ami-068990796e29243d5"
+  ami           = "ami-05ffe3c48a9991133"
   instance_type = "t2.micro"
 
   tags = {
