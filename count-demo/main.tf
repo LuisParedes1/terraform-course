56094ec5eccf8f01747723f90b0a53b776bd7fb9
Luis Paredes <lparedesr@fi.uba.ar>
2025-07-09 15:58:08 -0300

    simple count demo

---
+++ count-demo/main.tf
@@ -0,0 +1,15 @@
+# Count attribute allows us to create multiple resources.
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "number_instances" {
+  type    = number
+  default = 3
+}
+
+resource "aws_instance" "db" {
+  ami           = "ami-05ffe3c48a9991133"
+  instance_type = "t2.micro"
+  count         = var.number_instances
+}
